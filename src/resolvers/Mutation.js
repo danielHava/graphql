30a8dbb2102a17a60edbfe1c61a49cba8a12980e
Daniel Hava <dani.hava@protonmail.com>
2019-07-04 16:18:22 +0300

    fix import of bcrypt and added index file

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const { APP_SECRET, getUserId } = require('../utils')
 
 function createLink (parent, args, context, info) {
