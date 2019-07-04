d0f5f6cb4acfb3911055a75f6e6857ed7eed95a9
Daniel Hava <dani.hava@protonmail.com>
2019-07-04 15:29:30 +0300

    refactor index to use imports

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-const typeDefs = `
-  type Query{
-      info: String!
-      err: String!
-  }
-`
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    err: () => null
-  }
-}
+const resolvers = require('./resolvers')
+const { prisma } = require('./generated/prisma-client')
 
 const server = new GraphQLServer({
-  typeDefs,
-  resolvers
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: request => { 
+    return { 
+      ...request,
+      prisma
+    }
+  }
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
